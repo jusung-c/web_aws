c38864f430947e8f27bbf1a39f8a8b34125786b9
jusung-c <anaooauc1236@naver.com>
2023-10-08 19:36:15 +0900

    test(#2): 등록 API 테스트

--- src/test/java/com/example/web_aws/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/example/web_aws/domain/posts/PostsRepositoryTest.java
@@ -1,28 +1,25 @@
 package com.example.web_aws.domain.posts;
 
-import org.aspectj.lang.annotation.After;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-class PostRepositoryTest {
+class PostsRepositoryTest {
     @Autowired
-    PostRepository postRepository;
+    PostsRepository postsRepository;
 
     @AfterEach
     public void cleanup() {
-        postRepository.deleteAll();
+        postsRepository.deleteAll();
     }
 
     @Test
@@ -31,9 +28,9 @@ class PostRepositoryTest {
         String title = "test 제목";
         String content = "test 본문";
 
-        postRepository.save(Posts.builder().title(title).content(content).author("hi").build());
+        postsRepository.save(Posts.builder().title(title).content(content).author("hi").build());
 
-        List<Posts> postsList = postRepository.findAll();
+        List<Posts> postsList = postsRepository.findAll();
 
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
