ee060b439a1cfe56b2ee9427aeafbb0560b452ed
jusung-c <anaooauc1236@naver.com>
2023-10-08 20:20:22 +0900

    test(#2): 수정 API 테스트

--- src/test/java/com/example/web_aws/web/PostsApiControllerTest.java
+++ src/test/java/com/example/web_aws/web/PostsApiControllerTest.java
@@ -3,13 +3,17 @@ package com.example.web_aws.web;
 import com.example.web_aws.domain.posts.Posts;
 import com.example.web_aws.domain.posts.PostsRepository;
 import com.example.web_aws.web.dto.PostsSaveRequestDto;
+import com.example.web_aws.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -37,6 +41,7 @@ public class PostsApiControllerTest {
     }
 
     @Test
+    @DisplayName("Post Save Api Success Test")
     public void Posts_등록() throws Exception {
         // given
         String title = "title";
@@ -60,5 +65,40 @@ public class PostsApiControllerTest {
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
         assertThat(all.get(0).getAuthor()).isEqualTo("lee");
+    }
+
+    @Test
+    @DisplayName("Post update Api Success Test")
+    public void Posts_수정된다() throws Exception {
+        // given
+        Posts savePost = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        Long updateId = savePost.getId();
+        String updateTitle = "title2";
+        String updateContent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(updateTitle)
+                .content(updateContent).build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        // when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(updateTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(updateContent);
+
     }
 }
